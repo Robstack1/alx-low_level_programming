aa5920994c1282a82e6f9af5e733571a412261bc
Robstack1 <roflorent@gmail.com>
2023-08-10 07:50:30 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,23 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *
+ * main - to print the size of dara types
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
+	printf("size of a char: %lu", sizeof(a));
+	printf("size of an int: %lu", sizeof(b));
+	printf("size of a long int: %lu", sizeof(c));
+	printf("size of a long long int: %lu", sizeof(d));
+	printf("size of a float: %lu", sizeof(e));
+	return (0);
+}
