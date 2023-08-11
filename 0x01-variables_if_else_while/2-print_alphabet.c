7258717a4d55f7dca588d61e8cbad9f4cdfd9462
Robstack1 <roflorent@gmail.com>
2023-08-11 15:20:41 -0700

    Lower case alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char i = 'a';
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	while (i <= 'z');
+	for (i = 0; i < 26; i++)
 	{
-		putchar(i);
-		i++;
+		putchar(a;p[i]);
 	}
 	putchar('\n');
 	return (0);
