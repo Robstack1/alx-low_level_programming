056490aa51781644e4bac4caac55a61fdf1fb36c
Robstack1 <roflorent@gmail.com>
2023-08-11 16:31:18 -0700

    Robstack

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible different combinations of three digits
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	int n, m, o;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (o = 50; o < 58; 0++)
+			{
+				if (o > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(o);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
