b87d068ad32369a37794f66acc6698f2cdd8203e
Robstack1 <roflorent@gmail.com>
2023-08-11 15:34:49 -0700

    Upper case alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,18 +7,12 @@
  */
 int main(void)
 {
-	char Lower = 'a';
-	char Upper = 'A';
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-	while (Lower <= 'z')
+	for (i = 0; i < 52; i++)
 	{
-		putchar(Lower);
-		Lower++;
-	}
-	while (Upper);
-	{
-		putchar(Upper);
-		Upper++;
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
