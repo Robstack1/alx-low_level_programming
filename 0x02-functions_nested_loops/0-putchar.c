17ad0e57abd959bce56f1988a731fd80306a4c25
Robstack1 <roflorent@gmail.com>
2023-08-15 07:54:19 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - print _putchar
- *
- * Return: (0)
- *
+ * is written by mbah
+ * Return: 0
  */
 int main(void)
 {
-	int i;
-
-	char c[] = "_putchar";
-
-	for (i = 0; i <= 7; i++)
-	{
-		putchar(c[i]);
-	}
-	putchar ('\n');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,1 @@
-0x02. C - Functions, nested loops
-
-TASK TO BE DONE
+0-putchar
