e105ebb4bb09ccdf1d1b7eff7369148dff47abb3
Robstack1 <roflorent@gmail.com>
2023-08-24 15:43:27 -0700

    Strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,9 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	int len_dest, i;
 
 	for (len_dest = 0; dest[len_dest] != '\0'; len_dest++)
-	{
-	
-	}
+	{}
 	for (i = 0; src[i] != 0 && i < n; i++)
 	{
 		dest[len_dest + i] = src[i];
